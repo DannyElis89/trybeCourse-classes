fac37843b4ffc27b208a513976a57ff1c05f7417
Danny Elis <dannyelis89@gmail.com>
2021-12-11 18:46:08 -0300

    exemplo 02 dia 5.2

--- modulo01/bloco05/dia02/exemplos/exemplo02.js
+++ modulo01/bloco05/dia02/exemplos/exemplo02.js
@@ -1,13 +1,34 @@
 // 1. Crie um irmão para elementoOndeVoceEsta .
 
+let elementoOndeEuTo = document.querySelector('#elementoOndeVoceEsta');
+
+let meuPai = elementoOndeEuTo.parentNode;
+let meuIrmao = document.createElement('section');
+
+meuIrmao.innerText = 'Meu irmão, filho do meu pai.';
+
+meuPai.appendChild(meuIrmao);
 
 
 // 2. Crie um filho para elementoOndeVoceEsta .
 
+let meuFilho = document.createElement('p');
+meuFilho.innerText = 'Parágrafo, meu filho!';
+
+elementoOndeEuTo.appendChild(meuFilho);
 
 
 // 3. Crie um filho para primeiroFilhoDoFilho .
 
+let primeiroNeto = document.querySelector('#primeiroFilhoDoFilho');
+let primeiroBisneto = document.createElement('p');
+primeiroBisneto.innerText = 'Primeiro bisneto!!!';
+
+primeiroNeto.appendChild(primeiroBisneto);
 
 
 // 4. A partir desse filho criado, acesse terceiroFilho .
+
+let meuBisavo = primeiroBisneto.parentNode.parentNode.parentNode;
+let acessandoTerceiroFilho = document.querySelector('#terceiroFilho');
+acessandoTerceiroFilho.innerText = 'Acessando o Elemento Terceiro filho.'
