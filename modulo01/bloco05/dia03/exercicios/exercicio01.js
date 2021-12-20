9e633187b4bcd9945b8d3e00dfb4e6ee7113fe66
Danny Elis <dannyelis89@gmail.com>
2021-12-20 02:27:55 -0300

    fim exercicios dia 5.3

--- modulo01/bloco05/dia03/exercicios/exercicio01.js
+++ modulo01/bloco05/dia03/exercicios/exercicio01.js
@@ -36,7 +36,7 @@ for(let index = 0; index < dezDaysList.length; index += 1){
 
   } else if (diaDoMes.innerText == '4' || diaDoMes.innerText == '11' ||  diaDoMes.innerText == '18'){
     let classeDiaDoMes = document.createAttribute('class');
-    classeDiaDoMes.value = 'friday';
+    classeDiaDoMes.value = 'day friday';
     diaDoMes.setAttributeNode(classeDiaDoMes);
 
   } else if(diaDoMes.innerText == '25'){
@@ -81,8 +81,15 @@ botao1.addEventListener('click', mudaBackgroundColor);
 
 function mudaBackgroundColor(){
   let feriados = document.getElementsByClassName('holiday');
+
   for (index = 0; index < feriados.length; index += 1 ){
-    feriados[index].style.backgroundColor = "yellow";
+    let diaFeriado = feriados[index];
+
+    if(diaFeriado.style.backgroundColor !== 'yellow'){
+      diaFeriado.style.backgroundColor = 'yellow';
+    } else if (diaFeriado.style.backgroundColor){
+      diaFeriado.style.backgroundColor = 'rgb(238,238,238)';
+    }
   }
 }
 
@@ -136,31 +143,139 @@ function mudaConteudo(){
 /*
 Exercício 6:
 */
+let diaDoMes = document.getElementsByClassName('day');
+let parametro = '';
 
+for(let i = 0; i < diaDoMes.length; i += 1){
+  parametro = diaDoMes[i].addEventListener('mouseover', zoomDia);
+  parametro = diaDoMes[i].addEventListener('mouseleave', zoomDia2);
+}
+
+function zoomDia (parametro){
+  // console.log(parametro.target)
+  let objeto = parametro.target;
+  objeto.style.transform = "scale(2)";
+}
+
+function zoomDia2 (parametro){
+  // console.log(parametro.target)
+  let objeto = parametro.target;
+  objeto.style.transform = "scale(1)";
+}
 
 /*
 Exercício 7:
 */
+let divMae1 = document.getElementsByClassName('my-tasks')[0];
+
+function adicionaTarefas (parametro){
+
+  let novaTarefa = document.createElement('span');
+  novaTarefa.innerHTML = parametro ;
+
+  divMae1.appendChild(novaTarefa);
+
+}
+
+  adicionaTarefas('Projetos');
 
 
 /*
 Exercício 8:
 */
 
+function corLegenda (parametroCor){
 
+  let divTask = document.createElement('div');
+
+  let classeDivTask = document.createAttribute('class');
+  classeDivTask.value = 'task';
+  divTask.setAttributeNode(classeDivTask);
+  divTask.style.backgroundColor = parametroCor;
+
+  divMae1.appendChild(divTask);
+
+}
+corLegenda('green');
 
 /*
 Exercício 9:
 */
+let divTask0 = document.getElementsByClassName('task')[0];
+divTask0.addEventListener('click', mudaClasseTask);
 
+function mudaClasseTask (){
+
+  if (!divTask0.classList.contains('selected')){
+    divTask0.classList.add("selected");
+  } else if (divTask0.classList.contains('selected')) {
+    divTask0.classList.remove("selected");
+  }
+}
 
 
 /*
 Exercício 10:
 */
 
+let diaDoMes1 = document.getElementsByClassName('day');
 
+for(let i = 0; i < diaDoMes1.length; i += 1){
+  diaDoMes1[i].addEventListener('click', mudaCorDia);
+}
+
+
+function mudaCorDia(parametroDiaClicado){
+ let diaClicado = parametroDiaClicado.target;
+ if(diaClicado.style.color !== divTask0.style.backgroundColor){
+  diaClicado.style.color = divTask0.style.backgroundColor;
+ } else {
+  diaClicado.style.color = 'rgb(119,119,119)';
+ }
+}
 
 /*
 Exercício 11: Bônus:
 */
+
+let novaLista = document.createElement('ul');
+let divMae2 = document.getElementsByClassName('input-container')[0];
+let tituloH3 = divMae2.firstElementChild;
+
+tituloH3.appendChild(novaLista);
+
+let botaoAdcCompromisso = document.getElementById('btn-add');
+let caixaCompromisso = document.getElementById('task-input');
+
+botaoAdcCompromisso.addEventListener('click', adcCompromisso1)
+
+function adcCompromisso1 (){
+  let conteudoCaixaDeTexto = caixaCompromisso.value
+  // console.log(event)
+
+  if(conteudoCaixaDeTexto.length == 0){
+    window.alert('Erro! Digite o compromisso para então adicioná-lo á lista.');
+    return
+  } //else {
+    let novoItemDeLista = document.createElement('li');
+
+    novoItemDeLista.innerText = caixaCompromisso.value;
+    caixaCompromisso.value = '';
+    divMae2.appendChild(novoItemDeLista);
+  //}
+}
+
+caixaCompromisso.addEventListener('keyup', function(event) {
+  let conteudoCaixaDeTexto = caixaCompromisso.value
+  if(conteudoCaixaDeTexto.length == 0){
+    window.alert('Erro! Digite o compromisso para então adicioná-lo á lista.');
+    return
+  }
+  if (event.key === 'Enter'){
+    let novoItemDeLista = document.createElement('li');
+
+    novoItemDeLista.innerText = caixaCompromisso.value;
+    caixaCompromisso.value = '';
+    divMae2.appendChild(novoItemDeLista);
+  }
+})
