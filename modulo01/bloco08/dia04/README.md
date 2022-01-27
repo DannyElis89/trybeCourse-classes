5000edd0b9a4c9d0e2bf22b937f110436587a649
Danny Elis <dannyelis89@gmail.com>
2022-01-27 01:34:46 -0300

    exercicios dia 8.4

---
+++ modulo01/bloco08/dia04/README.md
@@ -0,0 +1,4 @@
+# Bloco 8: Higher Order Functions do JavaScript ES6
+## Dia 04: JavaScript ES6 - Higher Order Functions - reduce
+### Conteúdos abordados:
+* Reduce;
---
+++ modulo01/bloco08/dia04/exercicios/exercicio01.js
@@ -0,0 +1,13 @@
+// Dada uma matriz, transforme em um array.
+const arrays = [
+  ['1', '2', '3'],
+  [true],
+  [4, 5, 6],
+];
+
+function flatten() {
+  // escreva seu código aqui
+  return arrays.reduce((newArray, item) => newArray.concat(item), []);
+}
+
+console.log(flatten())
