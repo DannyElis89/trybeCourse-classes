23144a62f23eae21a500e6df1416179a23a7d321
DannyElis <dannyelis89@gmail.com>
2022-02-18 19:26:42 -0300

    exemplo 04 dia 10.2

--- modulo02/bloco10/dia02/exemplos/albuns-list/src/App.js
+++ modulo02/bloco10/dia02/exemplos/albuns-list/src/App.js
@@ -3,41 +3,43 @@ import Album from './components/Album'
 
 class App extends React.Component {
   render() {
-    const album01 = {
-      image: 'https://upload.wikimedia.org/wikipedia/en/thumb/b/b0/Coldplay_-_Mylo_Xyloto.JPG/220px-Coldplay_-_Mylo_Xyloto.JPG',
-      title: 'Mylo Xyloto',
-      releaseDate: {
-        year: '2011',
-        month: '10',
-        day: '24',
+    const albums = [
+      {
+        image: 'https://upload.wikimedia.org/wikipedia/en/thumb/b/b0/Coldplay_-_Mylo_Xyloto.JPG/220px-Coldplay_-_Mylo_Xyloto.JPG',
+        title: 'Mylo Xyloto',
+        releaseDate: {
+          year: '2011',
+          month: '10',
+          day: '24',
+        },
+        others: {
+          recordCompany: 'Capitol, Parlophone',
+          formats: 'CD, digital'
+        }
       },
-      others: {
-        recordCompany: 'Capitol, Parlophone',
-        formats: 'CD, digital'
-      }
-    };
-
-    const album02 = {
-      image: 'https://upload.wikimedia.org/wikipedia/pt/thumb/5/5d/Coldplay_-_Ghost_Stories.jpg/220px-Coldplay_-_Ghost_Stories.jpg',
-      title: 'Ghost Stories',
-      releaseDate: {
-        year: '2014',
-        month: '05',
-        day: '16',
+      {
+        image: 'https://upload.wikimedia.org/wikipedia/pt/thumb/5/5d/Coldplay_-_Ghost_Stories.jpg/220px-Coldplay_-_Ghost_Stories.jpg',
+        title: 'Ghost Stories',
+        releaseDate: {
+          year: '2014',
+          month: '05',
+          day: '16',
+        },
+        others: {
+          recordCompany: 'Parlophone',
+          formats: 'CD, digital'
+        }
       },
-      others: {
-        recordCompany: 'Parlophone',
-        formats: 'CD, digital'
-      }
-    };
+    ];
 
     return (
-      <div>
-        <Album album={ album01 } />
-        <Album album={ album02 } />
-      </div>
-    );
+      albums.map(album => (
+        <div>
+          <Album album={ album } />
+        </div>
+      ))
+    )
   }
-}
+};
 
 export default App;
